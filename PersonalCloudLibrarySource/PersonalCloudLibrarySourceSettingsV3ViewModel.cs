3b4f29db5f0ce15cfc67b45703b7fb507b5f8d66
KurtusCobain <73853854+KurtusCobain@users.noreply.github.com>
2026-07-12 17:41:07 -0400

    Prevent settings editor from downgrading V3 settings

--- PersonalCloudLibrarySource/PersonalCloudLibrarySourceSettingsV3ViewModel.cs
+++ PersonalCloudLibrarySource/PersonalCloudLibrarySourceSettingsV3ViewModel.cs
@@ -1,12 +1,24 @@
+using Playnite.SDK;
+using Playnite.SDK.Data;
+using System.Collections.Generic;
+
 namespace PersonalCloudLibrarySource
 {
-    public sealed class PersonalCloudLibrarySourceSettingsV3ViewModel : PersonalCloudLibrarySourceSettingsViewModel
+    public sealed class PersonalCloudLibrarySourceSettingsV3ViewModel : PersonalCloudLibrarySourceSettingsViewModel, ISettings
     {
+        private readonly PersonalCloudLibrarySource plugin;
+        private PersonalCloudLibrarySourceSettingsV3 editingClone;
+
         public PersonalCloudLibrarySourceSettingsV3ViewModel(PersonalCloudLibrarySource plugin)
             : base(plugin)
         {
+            this.plugin = plugin;
+
             var loadedSettings = plugin.LoadPluginSettings<PersonalCloudLibrarySourceSettingsV3>();
-            var migration = SettingsMigrationService.Migrate(loadedSettings);
+            var sourceSettings = loadedSettings != null
+                ? (PersonalCloudLibrarySourceSettings)loadedSettings
+                : base.Settings;
+            var migration = SettingsMigrationService.Migrate(sourceSettings);
             base.Settings = migration.Settings;
 
             if (migration.WasMigrated)
@@ -17,8 +29,47 @@ namespace PersonalCloudLibrarySource
 
         public new PersonalCloudLibrarySourceSettingsV3 Settings
         {
-            get => (PersonalCloudLibrarySourceSettingsV3)base.Settings;
-            set => base.Settings = value;
+            get
+            {
+                var versionedSettings = base.Settings as PersonalCloudLibrarySourceSettingsV3;
+                if (versionedSettings != null)
+                {
+                    return versionedSettings;
+                }
+
+                var migration = SettingsMigrationService.Migrate(base.Settings);
+                base.Settings = migration.Settings;
+                plugin.SavePluginSettings(migration.Settings);
+                return migration.Settings;
+            }
+            set => base.Settings = value ?? new PersonalCloudLibrarySourceSettingsV3
+            {
+                SettingsVersion = PersonalCloudLibrarySourceSettingsV3.CurrentSettingsVersion
+            };
+        }
+
+        public new void BeginEdit()
+        {
+            editingClone = Serialization.GetClone(Settings);
+        }
+
+        public new void CancelEdit()
+        {
+            Settings = editingClone ?? SettingsMigrationService.Migrate(base.Settings).Settings;
+            editingClone = null;
+        }
+
+        public new void EndEdit()
+        {
+            base.Settings = Settings;
+            base.EndEdit();
+            editingClone = null;
+        }
+
+        public new bool VerifySettings(out List<string> errors)
+        {
+            base.Settings = Settings;
+            return base.VerifySettings(out errors);
         }
     }
 }
