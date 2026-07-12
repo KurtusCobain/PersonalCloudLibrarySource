71b96544b9691417b2341bcf685045fe5a5dd2eb
KurtusCobain <73853854+KurtusCobain@users.noreply.github.com>
2026-07-11 21:44:00 -0400

    Add dashboard view code-behind [skip ci]

---
+++ PersonalCloudLibrarySource/Dashboard/CloudLibraryDashboardView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace PersonalCloudLibrarySource
+{
+    public partial class CloudLibraryDashboardView : UserControl
+    {
+        public CloudLibraryDashboardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
