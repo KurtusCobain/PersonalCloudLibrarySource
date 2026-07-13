76fcd03ecd8dace423e409d36fbe3b0ede6ac8c1
KurtusCobain <73853854+KurtusCobain@users.noreply.github.com>
2026-07-12 21:26:03 -0400

    Isolate setup wizard theme contract

--- PersonalCloudLibrarySource.Tests/Program.cs
+++ PersonalCloudLibrarySource.Tests/Program.cs
@@ -8,7 +8,7 @@ namespace PersonalCloudLibrarySource.Tests
         public static int Main(string[] args)
         {
             var filteredArgs = args
-                .Concat(new[] { "--test=PersonalCloudLibrarySource.Tests.Ui.UiContractTests" })
+                .Concat(new[] { "--test=PersonalCloudLibrarySource.Tests.Ui.UiContractTests.SetupWizard_UsesPlayniteThemeForeground" })
                 .ToArray();
             return new AutoRun().Execute(filteredArgs);
         }
